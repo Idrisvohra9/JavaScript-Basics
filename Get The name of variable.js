4b1ccef569630351e1202c92b724c07582ba9eb2
Idris Haider <idrishaider987@gmail.com>
2023-01-19 20:29:29 +0530

    Delete Get The name of variable.js

--- Get The name of variable.js
+++
@@ -1,9 +0,0 @@
-// # This function is an arrow function that takes the variable as a parameter and and creates an object and returns the first key that being the name of the key in the object meaning the name of the variable.
-
-const varName = varObj => Object.keys(varObj)[0];
-
-const n = 42;
-
-const arr= [];
-
-console.log(varName({ arr }));
