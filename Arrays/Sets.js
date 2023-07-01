73bbf1c16f7416821687cfc906a8e83007f92703
Idrisvohra9 <idrishaider987@gmail.com>
2023-07-01 10:12:35 +0530

    Adding

---
+++ Arrays/Sets.js
@@ -0,0 +1,59 @@
+//# The Set object is a built-in data structure that allows you to store unique values of any type. It provides an efficient way to manage collections of values and offers various methods for working with the data stored in the set.
+
+// It is much like an array or an object but it stores unique values and has less methods, doesn't support indexing.
+
+//# Creating a Set:
+
+// $ Method 1: By passing an array into the constructor:
+
+const firstSet = new Set([1, 2, 3, 4, 5, 5]);// 5 is repeating
+
+// $ Method 2: By using .add() after initializing an empty set.
+
+const secondSet = new Set();
+
+secondSet.add(10);
+secondSet.add(9);
+secondSet.add(8);
+secondSet.add(8);// Here the value 8 already existed before and we have added it again in the second set
+secondSet.add(7);
+
+console.log("first ", firstSet);
+//// Output: first  Set(5) { 1, 2, 3, 4, 5 }
+// (We can see that Set ignores a value if it already exists)
+console.log("second ", secondSet);
+//// second  Set(4) { 10, 9, 8, 7 }
+
+console.log();
+
+// Deleting items
+console.log("deleting the first item in the first set");
+// we can do this by using .delete() method
+
+firstSet.delete(1);
+console.log("first ", firstSet);
+console.log("Clearing the second set");
+secondSet.clear();
+console.log("After clearing", secondSet);
+
+// Access a singular item:
+
+// If we want to access a certain item we have to convert the Set to an array
+
+console.log("Accessing item on index:0 in firstSet:", Array.from(firstSet)[0])
+console.log()
+// Length of a Set:
+
+console.log("The size of a first Set:", firstSet.size);
+console.log()
+
+// Checking the existence of an item:
+console.log("Checking the existence of an item in a Set")
+console.log("Does The first set contains 3?", firstSet.has(3) ? "yes" : "no");
+console.log("Does The first set contains 9?", firstSet.has(9) ? "yes" : "no");
+console.log()
+
+// .keys(), .values() .entries():
+
+
+// forEach()
--- Objects/Maps.js
+++ Objects/Maps.js
@@ -1,15 +1,102 @@
-// A map is similar to Objects as in it also stores data in key-value pairs.
+// # Map:
 
-// But in a Map the key can be of any data type.
+// | A map is similar to Objects as in it also stores data in key-value pairs.
 
-// A map remembers the original insertion order of the keys.
+// | But in a Map the key can be of any data type.
 
-// A map has a property that represents the size of the map.
+// | A map remembers the original insertion order of the keys meaning it can preserve the insertion order as the entries come they are appended at last.
 
-// It can be directly iterable unlike Objects.
+// | A map has a property that represents the size of the map.
 
-// Keys in Maps are ordered by their creation time.
+// | It can be directly iterable unlike Objects.
 
-// Do not have defalut keys.
+// | Keys in Maps are ordered by their creation time.
 
-    
+// | Do not have defalut keys.
+
+// | Maps can store any type of values as keys or values. This includes primitive types, objects, functions, and even other maps.
+
+// | Similar to Object, Keys within a Map are also unique, meaning each key can only exist once in the Map. If you try to set a value with an existing key, it will overwrite the previous value.
+
+//# Creating a Map:
+
+//// Method 1: by passing a 2d Array of entries:
+
+const firstMap = new Map([
+    ["Name", "Idris"],
+    [true, 10],
+    [{ age: 19 }, { gender: "Male" }],
+    [null, undefined],
+    [3.14, 69]
+]);
+
+//// Method 2: By set() method:
+
+const secondMap = new Map();
+// Syntax: mapObj.set(key, value);
+secondMap.set("Name", "Jay");
+secondMap.set(["Bruhh", "Sissy"], ["Freakin", "Idiot"])
+
+console.log("firstMap:");
+console.log(firstMap);
+console.log();
+// The keys and values in map are separated by arrow symbol '=>' rather than ':' a colon. This is really intresting..
+console.log("secondMap:");
+console.log(secondMap);
+console.log()
+
+console.log("Size of firstMap:");
+console.log(firstMap.size);
+console.log();
+
+
+// Get properties:
+
+console.log("Get property Name: ", firstMap.get("Name"));
+
+const testMap = new Map([
+    ["first", 1],
+    [2, "second"],
+    [3.0, "third"],
+]);
+
+console.log("test map: ", testMap);
+console.log("After deleting the last property");
+testMap.delete(3.0);
+console.log(testMap);
+console.log()
+
+console.log("Check the existance of a property");
+// Check if map has a certain property:
+
+console.log("Does map have 4:", testMap.has(4) ? "yes" : "no");
+console.log("Does map have 2:", testMap.has(2) ? "yes" : "no");
+
+console.log();
+
+// Type of map
+console.log("Instance and type");
+console.log("typeof", typeof firstMap);
+console.log("Check if the variable is map:",firstMap instanceof Map ? "yes" : "no");
+console.log()
+
+// @ Iterating over a Map:
+console.log("Iterating over a Map:")
+
+// forEach loop:
+
+firstMap.forEach(function(value, key) {
+    console.log(key, value);
+});
+
+// we can also loop with .values(), .keys() or .entries()
+
+// This is all to Maps. Maps are particularly useful when you need to associate additional data or metadata with an object or when you require a more flexible and reliable key-value store.
+
+// Weak Maps vs Map:
+
+// In a Map, the keys are held as strong references, meaning they prevent the objects from being garbage collected as long as the Map exists.
+
+// WeakMap, on the other hand, only accepts objects as keys, and these keys are held as weak references. This means that if there are no other references to a key object, it can be garbage collected even if it is still present in the WeakMap.
+
+// Consequently, this makes WeakMap suitable for scenarios where you want to associate some metadata or auxiliary data with objects, but without preventing them from being collected by the garbage collector when no longer needed.
